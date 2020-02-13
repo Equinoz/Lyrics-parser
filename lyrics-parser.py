06dcd3c2eca21c94f84e4d81f65f825a2f677f9a
Equinoz <julien.lecoz33@gmail.com>
2020-02-13 10:45:13 +0100

    Calcul du score et affichage

--- lyrics-parser.py
+++ lyrics-parser.py
@@ -4,13 +4,13 @@
 import argparse
 import sys
 import re
+import math
 
 OUTPUT = "scores.log"
 
-""" Fonction vérifiant l'extension du fichier passé en argument """
-def checkExtension(file):
-  if file.split('.')[-1] != "txt":
-    print("Erreur: le fichier " + file + " doit posséder l'extension .txt")
+""" Gestion des erreurs """
+def errorHandler(text):
+    print(text)
     sys.exit()
 
 """ Fonction principale """
@@ -19,26 +19,39 @@ def main():
   parser = argparse.ArgumentParser(description="Script d'analyse de texte fournissant un indice: plus l'indice est proche de 1 plus le texte est varié, plus l'indice est proche de zéro plus le texte est répétitif")
   parser.add_argument("file", type=str, help="fichier .txt à analyser")
   parser.add_argument("-w", "--write", action="store_true", help="ajout du score du texte analysé dans le fichier " + OUTPUT)
-
   args = parser.parse_args()
 
   # Vérification de l'extension du fichier
-  checkExtension(args.file)
+  if args.file.split('.')[-1] != "txt":
+    errorHandler("Erreur: le fichier " + args.file + " doit posséder l'extension .txt")
 
   # Lecture du fichier
-  file = ""
   try:
-    with open(args.file, "r") as openedFile:
-      file = openedFile.read()
+    with open(args.file, 'r') as openedFile:
+      text = openedFile.read()
   except FileNotFoundError:
-    print("Erreur: le fichier nommé " + args.file + " n'as pas été trouvé")
-    sys.exit()
+    errorHandler("Erreur: le fichier nommé " + args.file + " n'as pas été trouvé")
 
-  # Suppression de la ponctuation et mise en minuscules
-  file = re.sub(r"([\.?;,\(\)\t])", r"", file)
-  file = re.sub(r"(['\n\r])", r" ", file)
-  file = re.sub(r"(\s{2,})", r" ", file).lower()
+  # Suppression de la ponctuation, des caractères spéciaux et mise en minuscules
+  text = re.sub(r"([\.?\"&{}/$€!:;,\(\)\t])", r'', text)
+  text = re.sub(r"(['\n\r])", r' ', text)
+  text = re.sub(r"(\s{2,})", r' ', text).lower()
 
-  print(file)
+  if text == "":
+    errorHandler("Erreur: le fichier " + args.file + " ne contient pas de texte")
+
+  # On splitte le texte et on supprime les mots de moins de 3 lettres, non significatifs
+  text = [word for word in text.split(' ') if len(word) > 2]
+
+  # On recense les mots différents
+  differentsWords = set(text)
+
+  # Calcul du score
+  score = math.ceil(len(differentsWords) / len(text) * 100) / 100
+
+  # Affichage du résultat
+  print("Nombre de mots de plus de 2 lettres: {}".format(len(text)))
+  print("Nombre de mots différents: {}".format(len(differentsWords)))
+  print("Score du texte: {}".format(score))
 
 main()
